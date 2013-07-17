3cb59397e1d6742f70851b38801b2f88396b5332
jcran <jcran@pentestify.com>
2013-07-17 16:48:41 -0500

    more cleanup

--- updates/latest.sh
+++ updates/latest.sh
@@ -1,22 +1,23 @@
-echo "downloading latest..."
+echo "[+] downloading latest..."
 wget http://pub.pwnieexpress.com/pwnplug_patch_1.1.3.tar.gz
 
-echo "checking patch hash..."
+echo "[+] checking patch hash..."
 hash=40cde500c51793865956a60e390fee262e9e5705
 sha1sum pwnplug_patch_1.1.3.tar.gz > temp_hash
 if [ `grep -o ${hash} temp_hash` ]; then
-    echo "extracting latest patch"
+    echo "[+] extracting latest patch"
     tar -zxvf pwnplug_patch_1.1.3.tar.gz
-    echo "running latest patch"
+    echo "[+] running latest patch"
     cd pwnplug_patch_1.1.3
-    chmod +x pwnplug_patch_1.1.3.sh
+    chmod +x ./pwnplug_patch_1.1.3.sh
     ./pwnplug_patch_1.1.3.sh
+    cd ..
 else
-  echo "unable to verify hash of latest patch"
+  echo "[-] unable to verify hash of latest patch"
 fi
 
 # Cleaning up... 
-echo "cleaning up..."
+echo "[+] cleaning up..."
 rm ./temp_hash
 
-echo "done"
+echo "[+] done"
